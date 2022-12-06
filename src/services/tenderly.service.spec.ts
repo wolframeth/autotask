c1d6748e0ad9e25631768de7a80660aa8b071bdb
wolframeth <0x200d@skiff.com>
2022-12-06 21:04:01 +0100

    Update; started working on treasury.servie.spec tests.

--- src/services/tenderly.service.spec.ts
+++ src/services/tenderly.service.spec.ts
@@ -1,39 +1,42 @@
 import { ethers } from 'ethers';
 import { EnvironmentsIdsEnum } from '../models/environments.enum';
 import { simulate } from './tenderly.service';
-describe('test', () => {
+describe('Tenderly services tests', () => {
   const vanityFromAddress = '0x96237A8958Cd96e3DEa180c1F64C244d383cAB39';
-  const badRequet = {
+  const invalidVanityFromAddress = '0x96237A8958Cd96e3DEa180c1F64C244d383cAB39';
+  const badRequest = {
     network_id: EnvironmentsIdsEnum.goerli,
-    from: vanityFromAddress,
+    from: invalidVanityFromAddress,
     input: '0x0',
     to: ethers.constants.AddressZero,
     block_number: null,
     save: true,
   };
-  const goodRequet = {
+  const goodRequest = {
     network_id: EnvironmentsIdsEnum.goerli,
     from: vanityFromAddress,
     input: '0x',
-    to: ethers.constants.AddressZero,
+    to: vanityFromAddress,
     block_number: null,
+    value: 1,
     save: true,
   };
   test('Expect tenderly to succeed.', async () => {
     const tenderlyTest = await simulate(
-      goodRequet.network_id,
-      goodRequet.from,
-      goodRequet.to,
-      goodRequet.input,
+      goodRequest.network_id,
+      goodRequest.from,
+      goodRequest.to,
+      goodRequest.input,
+      goodRequest.value,
     );
     expect(tenderlyTest).not.toEqual(false);
   });
   test('Expect tenderly to fail.', async () => {
     const tenderlyTest = await simulate(
-      badRequet.network_id,
-      badRequet.from,
-      badRequet.to,
-      badRequet.input,
+      badRequest.network_id,
+      badRequest.from,
+      badRequest.to,
+      badRequest.input,
     );
     expect(tenderlyTest).toEqual(false);
   });
--- src/services/treasury.service.spec.ts
+++ src/services/treasury.service.spec.ts
@@ -1,5 +1,22 @@
-describe('test', () => {
-  test('add', async () => {
-    expect(1 + 1).toEqual(2);
+import { ethers } from 'ethers';
+import { calculateEthRequiredForSwap, getEthUsdRate } from './treasury.service';
+import { EnvironmentsEnum } from '../models/environments.enum';
+import { isNumeric } from './misc.service';
+
+describe('Treasury services tests', () => {
+  const env = EnvironmentsEnum.GOERLI;
+  const provider = new ethers.providers.InfuraProvider('goerli');
+  test('Expect ETH/USD rate to be accurate', async () => {
+    const ethUsdRate = await getEthUsdRate(provider, env);
+    const isNumber = isNumeric(ethUsdRate as string);
+    expect(isNumber).toEqual(true);
+  });
+  test('Expect USD to ETH conversion', async () => {
+    const ethUsdRate = await getEthUsdRate(provider, env);
+    const ethConversion = calculateEthRequiredForSwap(
+      parseInt(ethUsdRate as string),
+      parseInt(ethUsdRate as string),
+    );
+    expect(ethConversion).toEqual(1);
   });
 });
