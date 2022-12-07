6f71a8c4020bee9f6074d58aef413131d4510856
wolframeth <0x200d@skiff.com>
2022-12-07 19:43:44 +0100

    Update; more tests provided.

--- src/services/treasury.service.spec.ts
+++ src/services/treasury.service.spec.ts
@@ -2,13 +2,17 @@ import { BigNumber, ethers } from 'ethers';
 import {
   calculateDifferenceBetweenBalanceAndDesiredAmont,
   calculateEthRequiredForSwap,
+  filterNonZeroStablecoinBalance,
+  filterStablcoinsBelowThreshold,
   getAllUSDBalance,
   getEthUsdRate,
+  stablecoinConfigurationToModel,
 } from './treasury.service';
 import { EnvironmentsEnum } from '../models/environments.enum';
 import { isNumeric } from './misc.service';
 import { StableCoinModel } from '../models/stablecoin.model';
 import { parseUnits } from 'ethers/lib/utils';
+import { generalConfigurations } from '../configurations/general.conf';
 
 describe('Treasury services tests', () => {
   const env = EnvironmentsEnum.GOERLI;
@@ -34,6 +38,15 @@ describe('Treasury services tests', () => {
       balance,
     );
     expect(ethers.BigNumber.from(9).toHexString()).toEqual(deficit);
+  });
+  test('Expect false if balance is sufficient', async () => {
+    const desiredAmount = ethers.BigNumber.from(10);
+    const balance = ethers.BigNumber.from(10);
+    const result = calculateDifferenceBetweenBalanceAndDesiredAmont(
+      desiredAmount,
+      balance,
+    );
+    expect(result).toEqual(false);
   });
   test('Expect stablecoin balances of given wallet', async () => {
     const stableCoins = [
@@ -66,6 +79,71 @@ describe('Treasury services tests', () => {
           );
         }
       }
+    }
+  });
+  test('Expect a list of stablecoins of which amount do not exceed the desired amount', async () => {
+    const stableCoins = {
+      USDC: {
+        token: 'USDC',
+        address: '0xd87ba7a50b2e7e660f678a895e4b72e7cb4ccd9c',
+        decimals: 'mwei',
+        balance: ethers.BigNumber.from(1),
+        desiredAmount: ethers.BigNumber.from(10),
+      },
+      DAI: {
+        token: 'DAI',
+        address: '0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60',
+        decimals: 'ether',
+        balance: ethers.BigNumber.from(10),
+        desiredAmount: ethers.BigNumber.from(10),
+      },
+    };
+    const qualifiedStablecoins = await filterStablcoinsBelowThreshold(
+      stableCoins,
+    );
+    expect(qualifiedStablecoins).not.toEqual(false);
+    expect(Object.keys(qualifiedStablecoins).length).toEqual(1);
+    expect(
+      'USDC' in (qualifiedStablecoins as { [token: string]: StableCoinModel }),
+    ).toEqual(true);
+  });
+
+  test('Expect a list of stablecoins of which balance is greater than 0', async () => {
+    const stableCoins = {
+      USDC: {
+        token: 'USDC',
+        address: '0xd87ba7a50b2e7e660f678a895e4b72e7cb4ccd9c',
+        decimals: 'mwei',
+        balance: ethers.BigNumber.from(0),
+      },
+      DAI: {
+        token: 'DAI',
+        address: '0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60',
+        decimals: 'ether',
+        balance: ethers.BigNumber.from(1),
+      },
+    };
+    const qualifiedStablecoins = filterNonZeroStablecoinBalance(stableCoins);
+    expect(qualifiedStablecoins).not.toEqual(false);
+    expect(Object.keys(qualifiedStablecoins).length).toEqual(1);
+    expect(
+      'DAI' in (qualifiedStablecoins as { [token: string]: StableCoinModel }),
+    ).toEqual(true);
+  });
+  test('Expect a list of stablecoins taken from configuration encapsulated in StableCoinsModel', async () => {
+    const stableCoins = generalConfigurations.stablecoinsAddresses[env];
+    const qualifiedStablecoins = stablecoinConfigurationToModel(env);
+    expect(qualifiedStablecoins).not.toEqual(false);
+    const qualifiedStablecoinsList = (
+      qualifiedStablecoins as StableCoinModel[]
+    ).map((qs) => {
+      return qs.token;
+    });
+    expect((qualifiedStablecoins as StableCoinModel[]).length).toEqual(
+      Object.keys(stableCoins).length,
+    );
+    for (const qs of Object.keys(stableCoins)) {
+      expect(qualifiedStablecoinsList.includes(qs)).toEqual(true);
     }
   });
 });
